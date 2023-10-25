68c5e08c2f51e543926c4e4954bcef77c230dd03
jschlecht <j.schlecht@inovex.de>
2023-10-25 13:24:42 +0200

    initial commit

---
+++ src/main/java/org/example/api/Commands.java
@@ -0,0 +1,9 @@
+package org.example.api;
+
+public interface Commands {
+    void createItem(String id);
+    void createItem(String id, int[] position, int value);
+    void deleteItem(String id);
+    void moveItem(String id, int[] vector);
+    void changeValue(String id, int newValue);
+}
---
+++ src/main/java/org/example/api/MovingItem.java
@@ -0,0 +1,8 @@
+package org.example.api;
+
+public interface MovingItem {
+    String getName();
+    int[] getLocation();
+    int getNumberOfMoves();
+    int getValue();
+}
