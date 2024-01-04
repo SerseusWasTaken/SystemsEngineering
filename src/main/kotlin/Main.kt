9c73dc4aafe6a91eb7828a1c02fdfacecdc0d8db
jschlecht <j.schlecht@inovex.de>
2024-01-04 16:00:12 +0100

    Aufgabe6/ Partitionen

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,9 +7,9 @@ import utils.RandomDataGenerator
 import kotlin.time.Duration.Companion.seconds
 
 fun main(args: Array<String>)  {
-    KafkaInitilizer.createTopic("data", 1)
+    KafkaInitilizer.createTopic("data", 4)
     val consumer = SpeedConsumer(30.seconds)
-    val producer = SpeedProducer(RandomDataGenerator(5, 1000,1000, 25.0), "data")
+    val producer = SpeedProducer(RandomDataGenerator(5, 1000,1000, 0.0), "data")
     runBlocking {
         launch {
             while (true) producer.produceData()
@@ -23,10 +23,9 @@ fun main(args: Array<String>)  {
         }
         launch {
             while (true) {
-                delay(32.seconds)
+                delay(40.seconds)
                 println("Average over time for 1,2,3: ${consumer.getAverageOfRoad(Clock.System.now() - 25.seconds, 1,2,3)}")
             }
         }
     }
-
 }
