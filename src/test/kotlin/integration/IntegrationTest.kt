5551fd75a42978a0d1d716953cec9d27786cd985
jschlecht <j.schlecht@inovex.de>
2024-01-22 12:25:03 +0100

    add example esper test

--- src/test/kotlin/integration/IntegrationTest.kt
+++ src/test/kotlin/integration/IntegrationTest.kt
@@ -1,30 +1,62 @@
 package integration
 
-import integration.di.TestModule
-import io.kotest.matchers.shouldBe
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.unmockkAll
-import kotlinx.coroutines.delay
+import com.espertech.esper.common.client.configuration.Configuration
+import com.espertech.esper.common.client.scopetest.EPAssertionUtil
+import com.espertech.esper.compiler.client.CompilerArguments
+import com.espertech.esper.compiler.client.EPCompileException
+import com.espertech.esper.compiler.client.EPCompilerProvider
+import com.espertech.esper.runtime.client.EPDeployException
+import com.espertech.esper.runtime.client.EPRuntime
+import com.espertech.esper.runtime.client.EPRuntimeProvider
+import com.espertech.esper.runtime.client.scopetest.SupportUpdateListener
+import esper.AverageSpeed
+import esper.FlattenedMeasurement
 import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.Clock
-import org.junit.jupiter.api.AfterEach
-import utils.Measurement
+import org.junit.jupiter.api.BeforeAll
 import kotlin.test.Test
-import kotlin.time.Duration.Companion.milliseconds
 
 
 class IntegrationTest {
 
-    @AfterEach
-    fun teardown() {
-        unmockkAll()
-        clearAllMocks()
+    @Throws(EPDeployException::class, EPCompileException::class)
+    fun setStatement(statement: String): SupportUpdateListener {
+        val compiled = EPCompilerProvider.getCompiler().compile(statement, compilerArgs)
+        val stmt = runtime.deploymentService.deploy(compiled).statements[0]
+        val listener = SupportUpdateListener()
+        stmt.addListener(listener)
+        return listener
     }
 
     @Test
-    fun `values from the future should not fall into current time window`(): Unit = runBlocking {
+    fun `should calculate average correctly`(): Unit = runBlocking {
+        val q5 = "@name('getAverage') select avg(speed) as averageSpeed, sensor from FlattenedMeasurement group by sensor having count(speed) > 0;\n"
+        val listener = setStatement(q5)
+        val time = Clock.System.now()
+        runtime.eventService.sendEventBean(
+            FlattenedMeasurement(time.toEpochMilliseconds(), 1, 20.0),
+            "FlattenedMeasurement"
+        )
+        runtime.eventService.sendEventBean(
+            FlattenedMeasurement(time.toEpochMilliseconds() + 1, 1, 40.0),
+            "FlattenedMeasurement"
+        )
+        EPAssertionUtil.assertProps(
+            listener.lastNewData[0],
+            arrayOf("averageSpeed", "sensor"), arrayOf(30.0, 1))
+    }
 
-
+    companion object {
+        val config = Configuration()
+        lateinit var compilerArgs: CompilerArguments
+        lateinit var runtime: EPRuntime
+        @JvmStatic
+        @BeforeAll
+        fun setup(): Unit {
+            config.common.addEventType(FlattenedMeasurement::class.java)
+            config.common.addEventType(AverageSpeed::class.java)
+            compilerArgs = CompilerArguments(config)
+            runtime = EPRuntimeProvider.getDefaultRuntime(config)
+        }
     }
 }
--- src/test/kotlin/integration/di/TestModule.kt
+++ src/test/kotlin/integration/di/TestModule.kt
@@ -12,14 +12,6 @@ import kotlin.time.Duration.Companion.milliseconds
 
 object TestModule {
 
-    val mockConsumer = mockk<Consumer>(relaxed= true)
 
-    val mockProducer = mockk<Producer>(relaxed= true)
-
-    val randomDataGenerator = spyk(RandomDataGenerator(1, 1, 1000, 0.0))
-
-    val speedConsumer = SpeedConsumer(10.milliseconds, mockConsumer)
-
-    val speedProducer = SpeedProducer(randomDataGenerator, "testTopic", mockProducer)
 
 }
